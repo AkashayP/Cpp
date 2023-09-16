80bb128e261e2f0d0a0eab8150745cbe2ddcacf5
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2023-09-17 01:04:33 +0530

    updationOfVariables.cpp

---
+++ collegeWallah/updationOfVariables.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout << x << endl;   // updation of variable
+    x = 8;
+    cout << x << endl;
+    x = 28;
+    cout << x << endl;
+    x = x-25;
+    cout << x << endl;
+    x = x*5;
+    cout << x << endl; 
+    x +=2;      // it means x= x+2
+    cout << x << endl;
+
+
+}
