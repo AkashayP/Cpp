58ad6c798864630816e0ccad9977f99f38a121eb
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2023-09-17 01:31:14 +0530

    areaofCircle.cpp

---
+++ collegeWallah/areaofCircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r = 8;
+    float area = 3.14159265359*r*r;
+    cout <<"Area is :  "<< area <<endl;
+
+
+    // also we can follow this approach but we have to write readable codes to increase readability.
+
+    cout <<3.14159265359*r*r;
+}
