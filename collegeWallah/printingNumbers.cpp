af3b067af958d09966ed35350d60c197b9a98e4a
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2023-09-16 23:24:54 +0530

    printingNumbers.cpp

---
+++ collegeWallah/printingNumbers.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+// int main(){
+//     cout<<"4+6"<<endl;   //If we write anything between double inverted comma it will print as it is
+
+//     cout<<4+6<<endl;   // If anything written without commas it will we be like a command.
+//     cout<<4*3<<endl;    
+// }
+
+// Variables and their Declarations.
+
+int main(){
+
+    int x ;    // Declaration 
+    x = 5;    //Initalization
+    cout<< x <<endl;
+
+    int y;
+    y = 9;
+
+    cout<<x+y<<endl;
+    cout << x+2 <<endl;
+    cout << y-2 <<endl;
+
+
+}
