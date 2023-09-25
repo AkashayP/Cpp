578e7b638beb4b9bea933dd9bc61fd5bfc7de282
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2023-09-26 01:28:37 +0530

    divisibleBy5.cpp

---
+++ collegeWallah/Chapter2/divisibleBy5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    if(n%5==0) {
+        cout<<"Divisible by 5 ";
+        
+    }
+    else {
+        cout<<"Not Divisible";
+        
+    }
+    
+}
