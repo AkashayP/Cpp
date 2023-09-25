2ad37ab4b15c7063565cf3fd070ded24c97ce3c7
Akashay Prakash Singh <112270350+AkashayP@users.noreply.github.com>
2023-09-25 23:24:46 +0530

    threeVariables.cpp

---
+++ collegeWallah/threeVariables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter 1st Number: "<<endl;
+    cin >> x;
+     int y;
+    cout<<"Enter 2nd Number: "<<endl;
+    cin >> y;
+     int z;
+    cout<<"Enter 3rd Number: "<<endl;
+    cin >> z;
+    
+    cout<< "Addition of x,y,z is : "<<x+y+z <<endl;
+    cout<< "Subtraction of x,y,z is : "<<x-y-z<<endl;
+    cout<< "Multiplication of x,y,z is : "<<x*y*z <<endl;
+}
